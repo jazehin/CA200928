ce2af2f88d9b4b6cb966c5b25212c2f833bee0e8
Kiss Bence <kiss.bence@vereb.dc>
2020-09-28 08:44:14 +0200

    Add project files.

---
+++ CA200928/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200928
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Hello World");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
